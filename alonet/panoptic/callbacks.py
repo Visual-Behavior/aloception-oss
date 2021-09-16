c78ff31a0094126dbc323bbcd753a5705e2ea70a
Johan MEJIA <johan.mejia@visualbehavior.ai>
2021-09-16 17:00:26 +0200

    Increase default DICE weights

--- alonet/panoptic/callbacks.py
+++ alonet/panoptic/callbacks.py
@@ -16,7 +16,8 @@ class PanopticObjectDetectorCallback(ObjectDetectorCallback):
         if trainer.logger is None:
             return
         if trainer.fit_loop.should_accumulate() or (trainer.global_step + 1) % (trainer.log_every_n_steps * 10) != 0:
-            return
+            if trainer.global_step % trainer.limit_train_batches != 0:
+                return
 
         assert isinstance(outputs, dict)
         assert "m_outputs" in outputs

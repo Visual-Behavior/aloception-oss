701cc01c6c9dd0cc0ecbf404e836ec45769bb4e1
Julien Salotti <julien.salotti@visualbehavior.ai>
2021-09-23 16:54:27 +0200

    fix timing (send frame to cuda)

--- alonet/raft/trt/timing.py
+++ alonet/raft/trt/timing.py
@@ -16,14 +16,15 @@ def get_sample_images(for_trt=False):
     dataset = ChairsSDHomDataset(sample=True)
     frame = next(iter(dataset.train_loader(sampler=SequentialSampler)))
     frame = SpatialAugmentedTensor.batch_list(frame)
-    frame = frame[:,:,:, 0:368, 0:496]
+    frame = frame[:, :, :, 0:368, 0:496]
     frame = frame.norm_minmax_sym()
     frame1 = frame[:, 0, :, :, :]
     frame2 = frame[:, 1, :, :, :]
-    if for_trt :
+    if for_trt:
         frame1 = frame1.as_tensor().numpy()
         frame2 = frame2.as_tensor().numpy()
     return frame1, frame2
+
 
 def load_trt_model(engine_path):
     load_trt_plugins_raft()
@@ -34,11 +35,12 @@ def load_trt_model(engine_path):
 
 def load_torch_model():
     model = RAFT(weights="raft-things")
-    model.eval()
+    model.eval().to("cuda")
     return model
 
+
 def timing_trt(engine_path=None):
-    if engine_path is None :
+    if engine_path is None:
         engine_path = os.path.join(ALONET_ROOT, "raft-things_fp32.engine")
 
     model = load_trt_model(engine_path)
@@ -55,12 +57,12 @@ def timing_trt(engine_path=None):
     times = []
     print("Executing engine for timing...")
     for i in range(N):
-        print(f"  forward pass {i+1}/{N}" )
+        print(f"  forward pass {i+1}/{N}")
         tic = time.time()
         model.execute()
         times.append(time.time() - tic)
 
-    times = (np.array(times) * 1000).astype(np.int) # ms
+    times = (np.array(times) * 1000).astype(np.int)  # ms
     print("Engine execution time:")
     print(f"  min : {times.min()} ms")
     print(f"  median: {int(np.median(times))} ms")
@@ -69,6 +71,8 @@ def timing_trt(engine_path=None):
 
 def timing_torch():
     frame1, frame2 = get_sample_images(for_trt=False)
+    frame1 = frame1.to("cuda")
+    frame2 = frame2.to("cuda")
     model = load_torch_model()
 
     # GPU Warm-up
@@ -79,18 +83,17 @@ def timing_torch():
     times = []
     print("Executing torch model for timing...")
     for i in range(N):
-        print(f"  forward pass {i+1}/{N}" )
+        print(f"  forward pass {i+1}/{N}")
         tic = time.time()
         model(frame1, frame2, only_last=True)
         times.append(time.time() - tic)
 
-    times = (np.array(times) * 1000).astype(np.int) # ms
+    times = (np.array(times) * 1000).astype(np.int)  # ms
     print("Torch execution time:")
     print(f"  min : {times.min()} ms")
     print(f"  median: {int(np.median(times))} ms")
     print(f"  max: {times.max()} ms")
-
-
+    print("\n", times)
 
 
 if __name__ == "__main__":

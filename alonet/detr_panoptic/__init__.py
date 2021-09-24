f0a9a239bfd7fa6b205cf57fc6431b425978785b
Johan MEJIA <johan.mejia@visualbehavior.ai>
2021-09-24 14:00:51 +0200

    merge into master

--- alonet/detr_panoptic/__init__.py
+++ alonet/detr_panoptic/__init__.py
@@ -1,5 +1,3 @@
-from .coco_data_modules import CocoPanoptic2Detr
-
 from .detr_panoptic import PanopticHead
 from .callbacks import PanopticObjectDetectorCallback
 from .train import LitPanopticDetr

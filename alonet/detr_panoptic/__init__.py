bcc6955178b8e1d291d8e9a7cf57083ecb4754f2
Johan MEJIA <johan.mejia@visualbehavior.ai>
2021-09-24 13:55:43 +0200

    delete panoptic criterion

--- alonet/detr_panoptic/__init__.py
+++ alonet/detr_panoptic/__init__.py
@@ -1,6 +1,5 @@
 from .coco_data_modules import CocoPanoptic2Detr
 
-from .criterion import PanopticCriterion
 from .detr_panoptic import PanopticHead
 from .callbacks import PanopticObjectDetectorCallback
 from .train import LitPanopticDetr

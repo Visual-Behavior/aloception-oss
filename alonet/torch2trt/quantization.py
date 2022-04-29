f6e6636589583e4d2c80ce37a58d3906bcb7c804
Taha <taha.azzim@gmail.com>
2022-04-29 13:11:19 +0200

    refactor: permute methods parameters

--- alonet/torch2trt/quantization.py
+++ alonet/torch2trt/quantization.py
@@ -1,79 +1,85 @@
+import os
 import torch
 import numpy as np
 import torch.nn as nn
+from tqdm import tqdm
 
-from pytorch_quantization import quant_modules
 from pytorch_quantization import nn as quant_nn
+from pytorch_quantization import quant_modules, calib
 from pytorch_quantization.tensor_quant import QuantDescriptor
 
 
 class QuantizedModel:
-    """Models quantization inteface : Transforms each model Layer to QuantLayer for quantization aware training
-    ConvTranspose2d ~ QuantConvTransposed2d, Conv2d ~ QuantConv2D, Linear ~ QuantLinear
-    
-    Parameters
-    ----------
-        calib_data: Iterable batched dataset
-            calibration data.
-        per_channel_quantization: bool.
-            either to quantize each channel or not.
-        method: str
-            method to use to compute maximum ("mse", "entropy", "max").
-        max_samples: int
-            maximum samples to use.
+    """Models quantization inteface
+    Transforms each model Layer to QuantLayer for quantization aware training.
+    Performs calibration for post training quantization.
     
     Exemples
     --------
         >>> from ... import Model
+        >>> from ... import Dataset
         >>> class QModel(QuantizedModel, Model):
         .       def __init__(self, ..., **kwargs):
         .           ## set QuantizedLayers description first
         .           self.set_default_desc()
         .           super(QModel).__init__(**kwargs)
         >>> quant_model = QModel(...)
-        >>> quant_model.calibrate()
+        >>> ## for QAT just fine tune the model as layers are converted
         >>> print(quant_model.cuda())
+        >>> ## for PTQ
+        >>> calib_data = Dataset()
+        >>> quant_model.calibrate(calib_data=calib_data, max_samples=10)
         
     """
-    def __init__(
-            self,
-            calib_data,
-            method="max",
-            max_samples=20,
-            calib_method='histogram',
-            per_channel_quantization=True,
-            **kwargs,
-            ):
-        self.method = method
-        self.calib_data = calib_data
-        self.max_samples = max_samples
-        self.calib_method = calib_method
-        self.per_channel_quantization = per_channel_quantization
-    
-    def set_default_desc(self, calib_method='histogram', per_channel_quantization=True):
-        quant_desc_weight = QuantDescriptor(calib_method=calib_method)
+    def set_default_desc(self,  calib_method="histogram", per_channel_quantization=True, **kwargs):
+        """Converts Layers to QuantLayers
+        
+        Parameters
+        ----------
+            calib_method: str
+                method to use to compute maximum ("histogram" | "max").
+            per_channel_quantization: bool.
+                either to calculate quzantization parameters for each channel or for the entire layer.
+        
+        raises
+        ------
+            ValueError
+                When calib_method is not one of "histogram" or "max".
+            
+        """
+        if calib_method not in ["histogram", "max"]:
+            ValueError("Unknown calibration method, should be max | histogram")
+            
         if per_channel_quantization:
-            quant_desc_input = QuantDescriptor(calib_method=calib_method)
+            quant_desc_input = QuantDescriptor(calib_method=calib_method, **kwargs)
         else:
-            quant_desc_input = QuantDescriptor(calib_method=calib_method, axis=None)
+            quant_desc_input = QuantDescriptor(calib_method=calib_method, axis=None, **kwargs)
+        quant_desc_weight = QuantDescriptor(calib_method=calib_method, **kwargs)
 
+        ## input
         quant_nn.QuantConvTranspose2d.set_default_quant_desc_input(quant_desc_input)
-        quant_nn.QuantConvTranspose2d.set_default_quant_desc_weight(quant_desc_weight)
-
         quant_nn.QuantConv2d.set_default_quant_desc_input(quant_desc_input)
-        quant_nn.QuantConv2d.set_default_quant_desc_weight(quant_desc_weight)
-
         quant_nn.QuantLinear.set_default_quant_desc_input(quant_desc_input)
+
+        ## weights
+        quant_nn.QuantConvTranspose2d.set_default_quant_desc_weight(quant_desc_weight)
+        quant_nn.QuantConv2d.set_default_quant_desc_weight(quant_desc_weight)
         quant_nn.QuantLinear.set_default_quant_desc_weight(quant_desc_weight)
 
         ## convert Layers to QuantLayers
         quant_modules.initialize()
 
-        ## set fake quantization to True before torch2onnx
-        quant_nn.TensorQuantizer.use_fb_fake_quant = True
-    
-    def collect_stats(self):
-        """Feed data to the network and collect statistic"""
+    def collect_stats(self, calib_data, max_samples=None):
+        """Feed data to the network and collect statistic
+        
+        Parameters
+        ----------
+            calib_data: Iterable batched dataset
+                calibration data.
+            max_samples: int
+                maximum samples to use.
+        """
+        max_samples = len(calib_data) if max_samples is None else max_samples
         # Enable calibrators
         for name, module in self.named_modules():
             if isinstance(module, quant_nn.TensorQuantizer):
@@ -83,15 +89,17 @@ class QuantizedModel:
                 else:
                     module.disable()
 
-        for i, image in enumerate(self.calib_data): ## FIXME
+        print('collecting stats...')
+        for i, image in tqdm(enumerate(calib_data), total=max_samples): ## FIXME
             if isinstance(image, np.ndarray):
                 image = np.expand_dims(image, axis=0)
+                image = torch.Tensor(image)
             self(image)
-            if i >= self.max_samples:
+            if i + 1>= max_samples:
                 break
 
         # Disable calibrators
-        for name, module in model.named_modules():
+        for name, module in self.named_modules():
             if isinstance(module, quant_nn.TensorQuantizer):
                 if module._calibrator is not None:
                     module.enable_quant()
@@ -105,12 +113,41 @@ class QuantizedModel:
             if isinstance(module, quant_nn.TensorQuantizer):
                 if module._calibrator is not None:
                     if isinstance(module._calibrator, calib.MaxCalibrator):
-                        module.load_calib_amax()
+                        ## setting strict to False for optimized model configuration
+                        module.load_calib_amax(strict=False)
                     else:
-                        module.load_calib_amax(**kwargs)
+                        ## setting strict to False for optimized model configuration
+                        module.load_calib_amax(strict=False, **kwargs)
+                print(F"Calibrator: {module._calibrator}")
+                print(F"{name:40}: {module}")
 
-    def calibrate(self):
-        """Calibrates the model"""
+    def calibrate(
+            self,
+            calib_data,
+            method="percentile",
+            percentile=99.99,
+            max_samples=10,
+            ):
+        """Calibrates the model
+        
+        Parameters
+        ----------
+            calib_data: Iterable dataset.
+                Calibration dataset.
+            method: int
+                Set when description calibrator is HistogramCalibrator. one of (mse | percentile).
+            percentile: float
+                Calibration percentile if calibrator is HistogramCalibrator. Ignored otherwise.
+            max_samples: int
+                Max samples to use for calibration.
+        
+        Raises
+        ------
+            ValueError
+                When method is not "mse" or "percentile"
+        """
+        if method not in ["mse", "percentile"]:
+            raise ValueError("Method should be \"mse\" | \"percentile\"")
         with torch.no_grad():
-            self.collect_stats()
-            self.compute_amax(method=self.method)
+            self.collect_stats(calib_data=calib_data, max_samples=max_samples)
+            self.compute_amax(method=method, percentile=percentile)

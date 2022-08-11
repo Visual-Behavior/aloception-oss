d908c9dc025b0f32a7af9bda1e74647f345d1430
Taha <taha.azzim@gmail.com>
2022-08-11 11:48:47 +0200

    fix: image load in Jetsonnx

--- aloscene/io/image.py
+++ aloscene/io/image.py
@@ -1,19 +1,19 @@
-import torchvision
-import torch
-from torchvision.io.image import ImageReadMode
-
 from aloscene.io.utils.errors import InvalidSampleError
+
+import cv2
+import torch
+import torchvision
+import numpy as np
+from torchvision.io.image import ImageReadMode
 
 
 def load_image(image_path):
     """
     Load an image with pytorch in float32 format
-
     Parameters
     ----------
     image_path : str
         path of the image
-
     Returns
     -------
     image : torch.Tensor
@@ -22,5 +22,10 @@ def load_image(image_path):
     try:
         image = torchvision.io.read_image(image_path, ImageReadMode.RGB).type(torch.float32)
     except RuntimeError as e:
-        raise InvalidSampleError(f"[Alodataset Warning] Invalid image: {image_path} error={e}")
+        try:
+            image = cv2.imread(image_path)
+            image = np.moveaxis(image, 2, 0)
+            image = torch.Tensor(image).type(torch.float32)
+        except RuntimeError as e:
+            raise InvalidSampleError(f"[Alodataset Warning] Invalid image: {image_path} error={e}")
     return image

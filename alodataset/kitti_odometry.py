38a7e28a189eb02fa173a39d41d7a9d09142fd90
Valentin Dury <valentin03dury@gmail.com>
2022-08-22 14:42:37 +0200

    Odometry Review
    - Sequence data on self.items.
    + Verify if the wanted sequances correspond to train or evaluation.
    
    Signed-off-by: Valentin Dury <valentin03dury@gmail.com>

--- alodataset/kitti_odometry.py
+++ alodataset/kitti_odometry.py
@@ -44,70 +44,81 @@ class KittiOdometryDataset(BaseDataset, SplitMixin):
 
         self.sequences = self._load_sequences(sequences)
 
-        self.items: Dict[Any, Any] = {"seq_params": {}}
+        self.items: Dict[Any, Any] = {}
+
+        self.seq_params = {}
 
         for seq in self.sequences:
-            # Conpute sequence parameters once and store them in the items dictionary.
-            self.items["seq_params"][seq] = {}
-            seq_params = self.items["seq_params"][seq]
 
             # Exploit data from calib.txt
             calib = self._load_calib(os.path.join(self.dataset_dir, "sequences", seq, "calib.txt"))
 
             # Register sequence parameters
-            seq_params["baseline"] = calib["b_gray" if self.grayscale else "b_rgb"]
-            seq_params["left_intrinsic"] = CameraIntrinsic(np.c_[calib[f"K_cam{0 if grayscale else 2}"], np.zeros(3)])
+            self.seq_params["baseline"] = calib["b_gray" if self.grayscale else "b_rgb"]
+            self.seq_params["left_intrinsic"] = CameraIntrinsic(
+                np.c_[calib[f"K_cam{0 if grayscale else 2}"], np.zeros(3)]
+            )
             if right_frame:
-                seq_params["right_intrinsic"] = CameraIntrinsic(
+                self.seq_params["right_intrinsic"] = CameraIntrinsic(
                     np.c_[calib[f"K_cam{1 if grayscale else 3}"], np.zeros(3)]
                 )
-            with open(os.path.join(self.dataset_dir, "sequences", seq, "times.txt"), "r") as f:
-                seq_params["times"] = [float(x) for x in f.readlines()]
 
+            with open(os.path.join(self.dataset_dir, "sequences", seq, "times.txt"), "r") as f:
+                times = [float(x) for x in f.readlines()]
+
+            poses = None
             if int(seq) < 11:
                 with open(os.path.join(self.dataset_dir, "poses", f"{seq}.txt"), "r") as f:
-                    seq_params["poses"] = f.readlines()
+                    poses = f.readlines()
 
             # Compute all the items.
-            sequence_size = len(seq_params["times"])
+            sequence_size = len(times)
 
             # Compute sequence indices
             temporal_sequences = sequence_indices(sequence_size, self.sequence_size, self.skip, self.sequence_skip)
             self.items.update(
                 {
                     len(self.items)
-                    - 1
                     + idx: {
                         "sequence": seq,
                         "temporal_sequence": temporal_seq,
+                        "times": [times[x] for x in temporal_seq],
+                        "poses": [poses[x] for x in temporal_seq] if poses else None,
                     }
                     for idx, temporal_seq in enumerate(temporal_sequences)
                 }
             )
 
     def __len__(self):
-        if self.sample:
-            return len(self.items)
-        return len(self.times)
+        return len(self.items)
 
     def _load_sequences(self, sequences) -> List[str]:
         loaded_sequences = []
 
         if sequences is None:
             for seq in sorted(os.listdir(os.path.join(self.dataset_dir, "sequences"))):
-                loaded_sequences.append(seq)
+                if (int(seq) <= 10 and self.split == Split.TRAIN) or (int(seq) >= 11 and self.split == Split.VAL):
+                    loaded_sequences.append(seq)
         elif isinstance(sequences, int):
             if not os.path.exists(os.path.join(self.dataset_dir, "sequences", f"{sequences:02d}")):
                 raise ValueError(f"Sequence {sequences:02d} does not exist")
+            if (sequences <= 10 and self.split == Split.VAL) or (sequences >= 11 and self.split == Split.TRAIN):
+                raise ValueError(f"Sequence {sequences:02d} is not in the {self.split} split")
             loaded_sequences.append(f"{sequences:02d}")
         elif isinstance(sequences, str):
             if not os.path.exists(os.path.join(self.dataset_dir, "sequences", sequences)):
                 raise ValueError(f"Sequence {sequences} does not exist")
+            if (int(sequences) <= 10 and self.split == Split.VAL) or (
+                int(sequences) >= 11 and self.split == Split.TRAIN
+            ):
+                raise ValueError(f"Sequence {sequences} is not in the {self.split} split")
             loaded_sequences.append(sequences)
         elif isinstance(sequences, list):
             for seq in sequences:
                 if not os.path.exists(os.path.join(self.dataset_dir, "sequences", seq)):
                     raise ValueError(f"Sequence {seq} does not exist")
+                if (int(seq) <= 10 and self.split == Split.VAL) or (int(seq) >= 11 and self.split == Split.TRAIN):
+                    raise ValueError(f"Sequence {seq} is not in the {self.split} split")
                 loaded_sequences.append(seq)
 
         return loaded_sequences
@@ -131,18 +142,13 @@ class KittiOdometryDataset(BaseDataset, SplitMixin):
             return BaseDataset.__getitem__(self, idx)
 
         item = self.items[idx]
-        print("item", item)
 
         left = []
         right = []
-        times = []
         poses = []
-        sequence = item["sequence"]
-        sequence_params = self.items["seq_params"][sequence]
+        sequence: str = item["sequence"]  # Number of the sequence
 
-        for seq in item["temporal_sequence"]:
-
-            times.append(float(sequence_params["times"][seq]))
+        for id, seq in enumerate(item["temporal_sequence"]):
 
             left.append(
                 Frame(
@@ -158,9 +164,7 @@ class KittiOdometryDataset(BaseDataset, SplitMixin):
 
             if self.split == Split.TRAIN:
                 pose = Pose(
-                    torch.Tensor([float(x) for x in sequence_params["poses"][seq].split(" ")] + [0, 0, 0, 1]).reshape(
-                        4, 4
-                    )
+                    torch.Tensor([float(x) for x in item["poses"][id].split(" ")] + [0, 0, 0, 1]).reshape(4, 4)
                 )
                 poses.append(pose)
 
@@ -179,17 +183,18 @@ class KittiOdometryDataset(BaseDataset, SplitMixin):
 
         frames: Dict[str, Frame] = {}
         frames["left"] = torch.cat(left, dim=0)
-        frames["left"].timestamp = times
-        frames["left"].baseline = sequence_params["baseline"]
-        frames["left"].append_cam_intrinsic(sequence_params["left_intrinsic"])
+        # time are incorrect
+        frames["left"].timestamp = item["times"]
+        frames["left"].baseline = self.seq_params["baseline"]
+        frames["left"].append_cam_intrinsic(self.seq_params["left_intrinsic"])
         if poses:
             frames["left"].append_pose(poses)
 
         if self.right_frame:
             frames["right"] = torch.cat(right, dim=0)
-            frames["right"].timestamp = times
-            frames["right"].baseline = sequence_params["baseline"]
-            frames["right"].append_cam_intrinsic(sequence_params["right_intrinsic"])
+            frames["right"].timestamp = item["times"]
+            frames["right"].baseline = self.seq_params["baseline"]
+            frames["right"].append_cam_intrinsic(self.seq_params["right_intrinsic"])
 
         return frames
 
@@ -269,5 +274,5 @@ class KittiOdometryDataset(BaseDataset, SplitMixin):
 if __name__ == "__main__":
     odo = KittiOdometryDataset(sequences=["00", "01"], sequence_skip=40, skip=28)
     r = odo.getitem(0)
-    print(r)
-    r["left"].get_view().render()
+    print(r["left"])
+    # r["left"].get_view().render()

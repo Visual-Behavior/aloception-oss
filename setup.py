90218d0d9f5c8f9e4ae974dcfc31f4e37ce7d421
Thomas Flahault <thomas.pro@tutanota.com>
2022-09-06 11:12:18 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='aloception',
+	author='Visual Behavior',
+	version='9.6.22',
+	description='Aloception is a set of package for computer vision: aloscene, alodataset, alonet.',
+	packages=find_packages(include=['aloscene', 'alodataset', 'alonet']),
+	url='https://github.com/Visual-Behavior/aloception',
+	install_requires=[
+		'numpy',
+		'opencv-python',
+		'torch',
+		'torchvision',
+		'matplotlib',
+		'Pillow',
+		'more-itertools',
+		'tensorflow',
+		'waymo-open-dataset',
+		'pytorch-lightning',
+		'pycocotools',
+		'click',
+		'nvidia-tensorrt',
+		'nvidia-pyindex',
+		'onnx',
+		'onnxsim',
+		'pycuda',
+		'wandb'
+	],
+	license_files=['LICENSE'],
+	keywords=['artificial intelligence', 'computer vision'],
+	classifiers=[
+		'Programming Language :: Python',
+		'Topic :: Scientific/Engineering :: Artificial Intelligence'
+	]
+)

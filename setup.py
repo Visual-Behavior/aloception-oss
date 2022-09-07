b9b624719000415a0ec0912f96f44490243d1796
Thomas Flahault <thomas.pro@tutanota.com>
2022-09-07 09:37:43 +0200

    tabs to spaces

--- setup.py
+++ setup.py
@@ -1,34 +1,34 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='aloception',
-	author='Visual Behavior',
-	version='0.3.0',
-	description='Aloception is a set of package for computer vision: aloscene, alodataset, alonet.',
-	packages=find_packages(include=['aloscene', 'alodataset', 'alonet']),
-	url='https://github.com/Visual-Behavior/aloception',
-	install_requires=[
-		'numpy',
-		'opencv-python',
-		'torch',
-		'torchvision',
-		'matplotlib',
-		'Pillow',
-		'more-itertools',
-		'tensorflow',
-		'pytorch-lightning',
-		'pycocotools',
-		'click',
-		'nvidia-tensorrt',
-		'onnx',
-		'onnxsim',
-		'pycuda',
-		'wandb'],
-	setup_requires=['nvidia-pyindex'],
-	license_files=['LICENSE'],
-	keywords=['artificial intelligence', 'computer vision'],
-	classifiers=[
-		'Programming Language :: Python',
-		'Topic :: Scientific/Engineering :: Artificial Intelligence'
-	]
+    name='aloception',
+    author='Visual Behavior',
+    version='0.3.0',
+    description='Aloception is a set of package for computer vision: aloscene, alodataset, alonet.',
+    packages=find_packages(include=['aloscene', 'alodataset', 'alonet']),
+    url='https://github.com/Visual-Behavior/aloception',
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'torch',
+        'torchvision',
+        'matplotlib',
+        'Pillow',
+        'more-itertools',
+        'tensorflow',
+        'pytorch-lightning',
+        'pycocotools',
+        'click',
+        'nvidia-tensorrt',
+        'onnx',
+        'onnxsim',
+        'pycuda',
+        'wandb'],
+    setup_requires=['nvidia-pyindex'],
+    license_files=['LICENSE'],
+    keywords=['artificial intelligence', 'computer vision'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ]
 )

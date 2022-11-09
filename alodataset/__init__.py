d5a0f1a5a72a2690a6a3f56a458602d85be2318d
Aurélien Cecille <aurelien.cecille@visualbehavior.ai>
2022-11-09 10:45:49 +0100

    minor fixes

--- alodataset/__init__.py
+++ alodataset/__init__.py
@@ -14,3 +14,4 @@ from .crowd_human_dataset import CrowdHumanDataset
 from .sintel_flow_dataset import SintelFlowDataset
 from .sintel_disparity_dataset import SintelDisparityDataset
 from .sintel_multi_dataset import SintelMultiDataset
+from .kitti_tracking import KittiTrackingDataset
--- alodataset/prepare/waymo_converter.py
+++ alodataset/prepare/waymo_converter.py
@@ -5,6 +5,9 @@ import numpy as np
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # only TF error messages are printed
 try:
     import tensorflow as tf
+    physical_devices = tf.config.list_physical_devices('GPU')
+    for device in physical_devices:
+        tf.config.experimental.set_memory_growth(device, True)
     import waymo_open_dataset
     import tqdm
 except:
